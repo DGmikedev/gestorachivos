ebb2ff0d5446027c8d1612d5605943173964e022
DGmike.dev <dgmike.dev@gmail.com>
2025-03-26 19:39:09 -0600

    starting factura mod

---
+++ gestorpdf/app/Http/Controllers/FacturaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Dompdf\Dompedf;
+use Dompdf\Options;
+
+class FacturaController extends Controller
+{
+    public function gen_factura(){
+
+        return "Hola mundo";
+
+    }
+}
--- gestorpdf/routes/web.php
+++ gestorpdf/routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dompdf_gen;
 use App\Http\Controllers\OrdenPago;
+use App\Http\Controllers\FacturaController;
 
 Route::get('/', function () {return view('welcome'); });
 Route::get('/dompdf',  [Dompdf_gen::class, "genera_dompdf"] );
 Route::get('/orden_pago', [OrdenPago::class, "genera_orden"]);
+Route::get('/factura', [FacturaController::class, "gen_factura"]);
